d1e8be6a2bcdf916262788821f989a34673308bb
Andrew <andrew.chan@chainsecurity.asia>
2025-05-14 13:50:59 +0800

    Remove backend codebase including main application files, services, controllers, and configuration.

--- backend/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import {} from "./app.interface";
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return "Hello World!";
-  }
-}
